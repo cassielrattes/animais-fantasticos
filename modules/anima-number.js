050f5d292f1ee8ed3e8aaf563f669424afdb1143
Cassiel Rattes Cortez <cassielrattes@gmail.com>
2021-12-15 15:51:39 -0300

    anima-number

--- modules/anima-number.js
+++ modules/anima-number.js
@@ -1,32 +1,47 @@
-export default function initAnimaNumber() {
-  const numeros = document.querySelectorAll("[data-numero]");
-
-  function animaNumeros() {
-    numeros.forEach((numero) => {
-      const total = +numero.innerText;
-      const incremento = Math.floor(total / 100);
-      let start = 0;
-      const timer = setInterval(() => {
-        start += incremento;
-        numero.innerText = start;
-        if (start > total) {
-          numero.innerText = total;
-          clearInterval(timer);
-        }
-      }, 25 * Math.random());
-    });
-  }
-
-  let observer;
-
-  function handleMutation(mutation) {
-    if (mutation[0].target.classList.contains("ativo")) {
-      observer.disconnect();
-      animaNumeros();
+export default class AnimaNumber {
+    constructor(numeros, observeTarget, observerClass) {
+        this.numeros = document.querySelectorAll(numeros);
+        this.observeTarget = document.querySelector(observeTarget);
+        this.observerClass = observerClass;
+        this.handleMutation = this.handleMutation.bind(this);
     }
-  }
 
-  const observeTarget = document.querySelector(".numeros");
-  observer = new MutationObserver(handleMutation);
-  observer.observe(observeTarget, { attributes: true });
+    static incrementarNumero(numero) {
+        const total = +numero.innerText;
+        const incremento = Math.floor(total / 100);
+        let start = 0;
+        const timer = setInterval(() => {
+            start += incremento;
+            numero.innerText = start;
+            if (start > total) {
+                numero.innerText = total;
+                clearInterval(timer);
+            }
+        }, 25 * Math.random());
+    }
+
+    animaNumeros() {
+        this.numeros.forEach((numero) =>
+            this.constructor.incrementarNumero(numero)
+        );
+    }
+
+    handleMutation(mutation) {
+        if (mutation[0].target.classList.contains(this.observerClass)) {
+            this.observer.disconnect();
+            this.animaNumeros();
+        }
+    }
+
+    addMutationObserver() {
+        this.observer = new MutationObserver(this.handleMutation);
+        this.observer.observe(this.observeTarget, { attributes: true });
+    }
+
+    init() {
+        if (this.numeros.length && this.observeTarget) {
+            this.addMutationObserver();
+        }
+        return this;
+    }
 }
--- modules/fetch-animais.js
+++ modules/fetch-animais.js
@@ -1,28 +1,33 @@
-import initAnimaNumber from "./anima-number.js";
+import AnimaNumber from "./anima-number.js";
 
 export default function initFetchAnimais() {
-  function createAnimal(animal) {
-    const div = document.createElement("div");
-    div.classList.add("numero-animal");
-    div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`;
-    return div;
-  }
-
-  async function fetchAnimais(url) {
-    try {
-      const animaisResponse = await fetch(url);
-      const animaisJSON = await animaisResponse.json();
-
-      const numerosGrid = document.querySelector(".numeros-grid");
-      animaisJSON.forEach((animal) => {
-        const divAnimal = createAnimal(animal);
-        numerosGrid.appendChild(divAnimal);
-      });
-      initAnimaNumber();
-    } catch (error) {
-      console.log(Error(error));
+    function createAnimal(animal) {
+        const div = document.createElement("div");
+        div.classList.add("numero-animal");
+        div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`;
+        return div;
     }
-  }
 
-  fetchAnimais("./animaisapi.json");
+    async function fetchAnimais(url) {
+        try {
+            const animaisResponse = await fetch(url);
+            const animaisJSON = await animaisResponse.json();
+
+            const numerosGrid = document.querySelector(".numeros-grid");
+            animaisJSON.forEach((animal) => {
+                const divAnimal = createAnimal(animal);
+                numerosGrid.appendChild(divAnimal);
+            });
+            const animaNumber = new AnimaNumber(
+                "[data-numero]",
+                ".numeros",
+                "ativo"
+            );
+            animaNumber.init();
+        } catch (error) {
+            console.log(Error(error));
+        }
+    }
+
+    fetchAnimais("./animaisapi.json");
 }
